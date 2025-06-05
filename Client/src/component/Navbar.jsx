a4ec87ef9922bb7dbab99aa337fe86f432a4c083
Deepesh <deepeshdangi700@gmail.com>
2025-06-05 23:08:13 +0530

    till 1:24

--- Client/src/component/Navbar.jsx
+++ Client/src/component/Navbar.jsx
@@ -41,6 +41,48 @@ const Navbar = () => {
                 </Link>
 
             </div>
+
+
+                <Link to="/profile">
+                    <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+                        <img src={thirdweb} alt="profile" className="w-[52px] h-[52px] rounded-full" />
+                    </div>
+                </Link>
+
+                 <div className='sm:hidden flex justify-between items-center relative'>
+               <div className= "w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+                <img src={thirdweb} alt="profile" className="w-[52px] h-[52px] rounded-full" />
+                </div> 
+                <img 
+                    src={menu} 
+                    alt="menu" 
+                    className="w-[34px] h-[34px] object-contain cursor-pointer" 
+                    onClick={() => setToggleDrawer((!toggleDrawer))} 
+                />
+                <div className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${!toggleDrawer ? 'translate-y-[100vh]': 'translate-y-0'} transition-all duration-500 ease-in-out`}>
+                    <ul className='mb-4'>
+                        {navlinks.map((link)=>(
+                            <li
+                                key={link.name}
+                                className={`flex p-4 ${isActive === link.name ? 'bg-[#3a3a43]' : ''} hover:bg-[#3a3a43] cursor-pointer`}
+                                onClick={() => {
+                                    setIsActive(link.name);
+                                    setToggleDrawer(false);
+                                    navigate(link.link);
+                                }}
+                            >
+                            <img
+                                src={link.imgurl}
+                                alt='link.name'
+                            />
+                            <p className={`ml-[20px] font-epilogue font-semibold text-[14px] ${isActive === link.name ? 'text-[#1dc071]': 'text-[#808191]'}`}>{link.name}</p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
+
+export default Navbar;
