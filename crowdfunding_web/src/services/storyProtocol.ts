6adbe5e3d36084f0326b3a45dce84eaad2d8bc3e
jeevant010 <jeevantmudgil10@gmail.com>
2025-06-15 17:56:00 +0530

    go all on master

--- crowdfunding_web/src/services/storyProtocol.ts
+++ crowdfunding_web/src/services/storyProtocol.ts
@@ -2,6 +2,13 @@ import { StoryProtocol } from "@story-protocol/core-sdk";
 import { createWalletClient, custom } from "viem";
 import { mainnet } from "viem/chains";
 
+// Add a declaration for window.ethereum
+declare global {
+  interface Window {
+    ethereum?: any;
+  }
+}
+
 export const getStoryProtocol = () => {
   if (!window.ethereum) throw new Error("Ethereum wallet not found");
 
